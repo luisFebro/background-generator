9640b038e71f33347d0a5a0e5bb658359b092944
Luis Febro <mr.febro@gmail.com>
2019-08-20 18:11:04 -0400

    new feature: button to copy to clipboard

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let endResultColor = document.querySelector(".endResultColor"),
+    color1 = document.querySelector(".color1"),
+    color2 = document.querySelector(".color2"),
+    body = document.getElementById("gradient"),
+    button = document.querySelector(".copyButton");
 
 
 function setGradient() {
-	body.style.background =
-	"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    body.style.background =
+        "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
 
-	css.textContent = body.style.background + ";";
+    endResultColor.value = body.style.background + ";";
 }
 
 color1.addEventListener("input", setGradient);
-
 color2.addEventListener("input", setGradient);
+
+// Copy to clipboard and tooltip
+
+
+function copyTextToClipboard(e) {
+    const copyText = document.querySelector(".endResultColor"),
+        tooltip = document.getElementById("myTooltip");
+    if(copyText.value === "Current CSS Background") {
+        e.preventDefault();
+        tooltip.innerHTML = "Select your desired color first!";
+    } else {
+        copyText.select();
+        document.execCommand("copy");
+        tooltip.innerHTML = "Copied to Clipboard";
+        setTimeout(() => tooltip.innerHTML = "", 4000);
+    }
+}
+
+function showTooltipOnmouseover() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
+
+button.addEventListener("click", copyTextToClipboard);
+button.addEventListener("mouseover", showTooltipOnmouseover);
